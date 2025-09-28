00e486d75c32406c9d2c50636c817014e62b1491
chojuninengu <chojuninengu@gmail.com>
2025-09-28 05:55:10 +0100

    modificataions stable

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         "background-dark": "#101722",
       },
       fontFamily: {
-        "display": ["Inter"]
+        "display": ["Inter", "sans-serif"]
       },
       borderRadius: {"DEFAULT": "0.25rem", "lg": "0.5rem", "xl": "0.75rem", "full": "9999px"},
     },
